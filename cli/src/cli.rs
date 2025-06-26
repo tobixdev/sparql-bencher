7921eca531f194ed8ddf36493660d9760e88b030
Tobias Schwarzinger <tobias.schwarzinger@tuwien.ac.at>
2025-06-26 16:35:54 +0200

    Fix project layout

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -1,12 +1,10 @@
-use clap::{Parser, Subcommand, ValueHint};
+use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(about, version, name = "rdf-fusion")]
 /// RdfFusion command line toolkit and SPARQL HTTP server
-pub struct Args {
-}
+pub struct Args {}
 
 #[derive(Subcommand)]
-pub enum Command {
-}
+pub enum Command {}
--- src/lib.rs
+++
@@ -1,5 +0,0 @@
-// src/lib.rs
-// This file is needed for integration tests or if you want to use this as a library.
-
-pub mod benchmark;
-pub mod subsystems;
